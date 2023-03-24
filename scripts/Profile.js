17ed2edcc3cb98ca7c0a94b4a77e97efc3dc828c
Tarasios <cwilliams153@my.bcit.ca>
2023-03-24 10:53:17 -0700

    Fully implemented profile editing

--- scripts/Profile.js
+++ scripts/Profile.js
@@ -1,61 +1,65 @@
 var currentUser;
 
 function populateUserInfo() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
-        if (user) {
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if user is signed in:
+    if (user) {
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("users").doc(user.uid);
+      //get the document for current user.
+      currentUser.get().then((userDoc) => {
+        //get the data fields of the user
+        var userName = userDoc.data().name;
+        var userProvince = userDoc.data().province;
+        var userCity = userDoc.data().city;
 
-            //go to the correct user document by referencing to the user uid
-            currentUser = db.collection("users").doc(user.uid)
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    //get the data fields of the user
-                    var userName = userDoc.data().name;
-                    var userProvince = userDoc.data().province;
-                    var userCity = userDoc.data().city;
-
-                    //if the data fields are not empty, then write them in to the form.
-                    if (userName != null) {
-                        document.getElementById("nameInput").value = userName;
-                    }
-                    if (userProvince != null) {
-                        document.getElementById("provinceInput").value = userProvince;
-                    }
-                    if (userCity != null) {
-                        document.getElementById("cityInput").value = userCity;
-                    }
-                })
-        } else {
-            // No user is signed in.
-            console.log ("No user is signed in");
+        //if the data fields are not empty, then write them in to the form.
+        if (userName != null) {
+          document.getElementById("nameInput").value = userName;
         }
-    });
+        if (userProvince != null) {
+          document.getElementById("provinceInput").value = userProvince;
+        }
+        if (userCity != null) {
+          document.getElementById("cityInput").value = userCity;
+        }
+      });
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+    }
+  });
 }
-
-//call the function to run it 
 populateUserInfo();
 
 function editUserInfo() {
-    //Enable the form fields
-    document.getElementById('personalInfoFields').disabled = false;
- }
+    document.getElementById("nameInput").disabled = false;
+    document.getElementById("countryInput").disabled = false;
+    document.getElementById("provinceInput").disabled = false;
+    document.getElementById("cityInput").disabled = false;
+  }
+  
 
- function saveUserInfo() {
-    //enter code here
-
-    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
-    userProvince = document.getElementById('provinceInput').value;     //get the value of the field with id="provinceInput"
-    userCity = document.getElementById('cityInput').value;       //get the value of the field with id="cityInput"
-
-    currentUser.update({
+function saveUserInfo() {
+    userName = document.getElementById("nameInput").value;
+    userCountry = document.getElementById("countryInput").value;
+    userProvince = document.getElementById("provinceInput").value;
+    userCity = document.getElementById("cityInput").value;
+  
+    currentUser
+      .update({
         name: userName,
+        country: userCountry;
         province: userProvince,
-        city: userCity
-})
-    .then(() => {
+        city: userCity,
+      })
+      .then(() => {
         console.log("Document successfully updated!");
-})
-
-document.getElementById('personalInfoFields').disabled = true; 
-}
+      });
+  
+    document.getElementById("nameInput").disabled = true;
+    document.getElementById("countryInput").disabled = true;
+    document.getElementById("provinceInput").disabled = true;
+    document.getElementById("cityInput").disabled = true;
+  }
+  
