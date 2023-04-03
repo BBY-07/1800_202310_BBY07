f1ec43bc93d56be888010747c475259616a229ff
Kevin Liang <kevinliang0303@gmail.com>
2023-04-03 04:11:44 -0700

    Add generateTaskCompleteUI function to create task UI elements

--- game/task.js
+++ game/task.js
@@ -18,6 +18,16 @@ const generateTaskUI = ({
   return task;
 };
 
+const generateTaskCompleteUI = ({ explanation = "explanation" }) => {
+  const taskHeader = `<div class="task-header">Task Complete</div>`;
+  const taskExplanation = `<div class="task-explanation">${explanation}</div>`;
+  const taskContinueButton = `<button onclick="handleContinue(event)">Continue</button>`;
+  const taskContinue = `<div class="task-continue">${taskContinueButton}</div>`;
+  const taskBody = `<div class="task-body">${taskExplanation}${taskContinue}</div>`;
+  const task = `<div class="task">${taskHeader}${taskBody}</div>`;
+  return task;
+};
+
 let inTask = false;
 let taskElement = null;
 let currentTask = null;
