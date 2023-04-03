00e0195a90f0377f3cd930e2317615faaf709706
unko-chan <kevin@kliang.dev>
2023-04-03 16:27:11 -0700

    Add hamburger menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,10 @@
-let zoom = false;
+function toggleMenu() {
+  const mainNav = document.getElementById('main-nav');
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
 
-document.getElementById("lawnmower").addEventListener("click", function () {
-  zoom = true;
-  backButton();
-  document.getElementById("background").style.transform = "scale(3)";
-  document.getElementById("lawnmower").style.opacity = 1;
-  document.getElementById("background-container").scrollTo({
-    top: 700,
-    left: 500,
-    behavior: "smooth",
-  });
-  document.getElementById("bottom").style.display = "block";
-});
+  // Toggle display of the nav menu
+  mainNav.style.display = mainNav.style.display === 'block' ? 'none' : 'block';
 
-const backButton = () => {
-  document.getElementById("back-button").style.display = "block";
-  document.getElementById("back-button").addEventListener("click", function () {
-    zoom = false;
-    document.getElementById("background").style.transform = "scale(1)";
-    document.getElementById("lawnmower").style.opacity = 0;
-    document.getElementById("back-button").style.display = "none";
-    document.getElementById("bottom").style.display = "none";
-
-  });
-};
+  // Toggle "x" class on the hamburger menu
+  hamburgerMenu.classList.toggle('x');
+}
