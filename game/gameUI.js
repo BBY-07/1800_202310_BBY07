cba570d9e6c4f2ad92bd47582012a08611a649e3
Kevin Liang <kevinliang0303@gmail.com>
2023-04-03 06:27:23 -0700

    Create gameUI

---
+++ game/gameUI.js
@@ -0,0 +1,23 @@
+let levelTasks = [];
+let levelName = null;
+let currentTask = 0;
+
+function generateGameUI (level) {
+  levelTasks = level.tasks;
+  levelName = level.name;
+
+  console.log(level)
+  const taskCounter = `<div class="task-counter">${currentTask}/${levelTasks.length}</div>`;
+  const taskTracker = `<div class="task-tracker">${levelTasks[currentTask].name}</div>`;
+  const gameUIContainer = `<div class="game-ui-container">${taskCounter}${taskTracker}</div>`;
+  const gameUI = createElement(gameUIContainer);
+  document.body.appendChild(gameUI);
+}
+
+updateUI = () => {
+  currentTask++;
+  const taskCounter = document.getElementsByClassName("task-counter")[0];
+  const taskTracker = document.getElementsByClassName("task-tracker")[0];
+  taskCounter.innerHTML = `${currentTask}/${levelTasks.length}`;
+  taskTracker.innerHTML = levelTasks[currentTask].name;
+}
