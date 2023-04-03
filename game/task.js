b7a16bce843c3ca85c904dd53a44c80101a0a321
Kevin Liang <kevinliang0303@gmail.com>
2023-04-03 03:27:33 -0700

    Implement basic task click functions

--- game/task.js
+++ game/task.js
@@ -1,3 +1,22 @@
+const generateTaskUI = ({
+  name,
+  description = "description",
+  action = "action",
+}) => {
+  const taskHeader = `<div class="task-header">${name}</div>`;
+  const taskDescription = `
+    <div class="task-description">
+      ${description}
+      <div class="what-to-do">What do you want to do?</div>
+    </div>
+  `;
+  const taskActionButton = `<button onclick="handleAction(event)">${action}</button>`;
+  const taskAction = `<div class="task-action">${taskActionButton}</div>`;
+  const taskBody = `<div class="task-body">${taskDescription}${taskAction}</div>`;
+  const task = `<div class="task">${taskHeader}${taskBody}</div>`;
+
+  return task;
+};
 
 let inTask = false;
 let taskElement = null;
