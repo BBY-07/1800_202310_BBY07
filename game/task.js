590e46f3f3e6ceac44f059a9838558af60d892ca
Kevin Liang <kevinliang0303@gmail.com>
2023-04-04 06:59:37 -0700

    Add completion animation

--- game/task.js
+++ game/task.js
@@ -16,7 +16,44 @@ const handleClickTask = (square) => {
 
 function handleAction(e) {
   e.preventDefault();
-  const correctAction = `<div class="task-correct">Correct!</div>`;
+  const correctAction = `
+
+  <div class="task-header">Task Complete!</div>
+    <div class="task-body">
+      <div class="w4rAnimated_checkmark">
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 130.2 130.2"
+        >
+          <circle
+            class="path circle"
+            fill="none"
+            stroke="#73AF55"
+            stroke-width="6"
+            stroke-miterlimit="10"
+            cx="65.1"
+            cy="65.1"
+            r="62.1"
+          />
+          <polyline
+            class="path check"
+            fill="none"
+            stroke="#73AF55"
+            stroke-width="6"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            points="100.2,40.2 51.5,88.8 29.8,67.5 "
+          />
+        </svg>
+      </div>
+    </div>
+
+  `;
+
+  // <div class="task">
+  
+  // </div>;
 
   updateUI();
 
@@ -37,5 +74,3 @@ function handleContinue(e) {
   inTask = false;
   zoomOut();
 }
-
-
