41bb4f97a9f892814acd47b56aeb62e792c5c3b2
Kevin Liang <kevinliang0303@gmail.com>
2023-04-04 07:48:38 -0700

    Add game end screen

--- game/gameUI.js
+++ game/gameUI.js
@@ -8,7 +8,11 @@ function generateGameUI(level) {
 
   console.log(level);
   const taskCounter = `<div class="task-counter">${currentTask}/${levelTasks.length}</div>`;
-  const taskTracker = `<div class="task-tracker">Task ${currentTask + 1}, ${levelTasks[currentTask].name}</div>`;
+  const taskTracker = `<div class="task-tracker">
+  <div class="current-task">
+  ${levelTasks[currentTask].name}
+  </div>
+  </div>`;
   const gameUIContainer = `<div class="game-ui-container">${taskCounter}${taskTracker}</div>`;
   const gameUI = createElement(gameUIContainer);
   document.body.appendChild(gameUI);
@@ -16,16 +20,30 @@ function generateGameUI(level) {
 
 updateUI = () => {
   currentTask++;
-  if (currentTask >= levelTasks.length) {
-    gameComplete();
-    return;
-  }
   const taskCounter = document.getElementsByClassName("task-counter")[0];
   const taskTracker = document.getElementsByClassName("task-tracker")[0];
   taskCounter.innerHTML = `${currentTask}/${levelTasks.length}`;
-  taskTracker.innerHTML = `Task ${currentTask + 1}, ${levelTasks[currentTask].name}`
+  if (currentTask >= levelTasks.length) {
+    return;
+  }
+  taskTracker.innerHTML = `<div class="current-task">${levelTasks[currentTask].name}</div>`;
 };
 
 function gameComplete() {
-  //implement game complete
+  console.log("game complete");
+  taskElement = createElement(`
+  <div class="task">
+  <div class="task-header">Good Job!</div>
+  <div class="task-body">
+    <div class="task-description">
+      <div class="description">You have completed the first level of FireSmart BC Homeowner's Prep!</div>
+      <div class="link">Click <a href="https://firesmartbc.ca/">here</a> to learn more about the potential impacts of wildfire on your home.</div>
+    </div>
+    <div class="task-continue">
+    <button onclick="handleRedirect(event)">Continue</button>
+  </div>
+</div>
+</div>
+  `);
+  document.body.appendChild(taskElement);
 }
