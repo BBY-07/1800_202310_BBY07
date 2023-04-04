0a8a55e8801c18ac5ddae938cbe1c359e06a27df
Kevin Liang <kevinliang0303@gmail.com>
2023-04-04 06:58:27 -0700

    Update task tracker

--- game/gameUI.js
+++ game/gameUI.js
@@ -2,13 +2,13 @@ let levelTasks = [];
 let levelName = null;
 let currentTask = 0;
 
-function generateGameUI (level) {
+function generateGameUI(level) {
   levelTasks = level.tasks;
   levelName = level.name;
 
-  console.log(level)
+  console.log(level);
   const taskCounter = `<div class="task-counter">${currentTask}/${levelTasks.length}</div>`;
-  const taskTracker = `<div class="task-tracker">${levelTasks[currentTask].name}</div>`;
+  const taskTracker = `<div class="task-tracker">Task ${currentTask + 1}, ${levelTasks[currentTask].name}</div>`;
   const gameUIContainer = `<div class="game-ui-container">${taskCounter}${taskTracker}</div>`;
   const gameUI = createElement(gameUIContainer);
   document.body.appendChild(gameUI);
@@ -23,8 +23,8 @@ updateUI = () => {
   const taskCounter = document.getElementsByClassName("task-counter")[0];
   const taskTracker = document.getElementsByClassName("task-tracker")[0];
   taskCounter.innerHTML = `${currentTask}/${levelTasks.length}`;
-  taskTracker.innerHTML = levelTasks[currentTask].name;
-}
+  taskTracker.innerHTML = `Task ${currentTask + 1}, ${levelTasks[currentTask].name}`
+};
 
 function gameComplete() {
   //implement game complete
